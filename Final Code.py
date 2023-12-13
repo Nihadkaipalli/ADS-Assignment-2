805964f12aa35f65eb67c02532c2b84b7c6eaf99
Nihadkaipalli <nihadkaipalli@gmail.com>
2023-12-13 16:46:38 +0000

    HeatMap

--- Final Code.py
+++ Final Code.py
@@ -169,14 +169,12 @@ def Neonatal_line(Neonatal):
 # Call the function and pass the CSV file path as an argument
 Neonatal_line('df_cleaned.csv')
 
-
 # Heatmap for India
-
 def India_HeatMap(Population):
     data = pd.read_csv(Population)
     India_data = data[data['Country Name'] == 'India']
 
-# Select relevant indicators
+    # Select relevant indicators
     indicators = [
         'Population, total',
         'Death rate, crude (per 1,000 people)',
@@ -187,49 +185,57 @@ def India_HeatMap(Population):
         'Number of infant deaths',
         'Cause of death, by non-communicable diseases (% of total)']
 
-# Create a subset of data with selected indicators
+    # Create a subset of data with selected indicators
     India_subset = India_data[indicators]
 
-# Plotting the heatmap
+    # Plotting the heatmap
     plt.figure(figsize=(12, 8))
     heatmap = sns.heatmap(India_subset.corr(), annot=True,
                           cmap='magma', fmt='.2f', annot_kws={"size": 10})
-    plt.title('Correlation Heatmap of Indicators for India')
-    plt.show()
 
+    # Adjusting x-axis and y-axis labels
+    plt.xticks(rotation=45, ha='right')  # Rotating x-axis labels by 45 degrees
+    plt.yticks(rotation=0)  # Keeping y-axis labels horizontal
+
+    plt.title('Correlation Heatmap of Indicators for India')
+    plt.tight_layout()  # Adjust layout for better visualization
+    plt.show()
 
 # Call the function and pass the CSV file path as an argument
 India_HeatMap('df_cleaned.csv')
 
-# Heatmap for United States
 
-
-def United_States_HeatMap(Population):
+# Heatmap for Mexico
+def Mexico_HeatMap(Population):
     data = pd.read_csv(Population)
-    United_States_data = data[data['Country Name'] == 'United States']
+    Mexico_data = data[data['Country Name'] == 'Mexico']
 
-# Select relevant indicators
+    # Select relevant indicators
     indicators = [
         'Population, total',
-        'Mortality caused by road traffic injury (per 100,000 population)',
-        'Suicide mortality rate (per 100,000 population)',
-        'Suicide mortality rate, male (per 100,000 male population)',
-        'Suicide mortality rate, female (per 100,000 female population)',
+        'Death rate, crude (per 1,000 people)',
+        'Life expectancy at birth, total (years)',
+        'Mortality rate, neonatal (per 1,000 live births)',
         'Mortality rate, infant (per 1,000 live births)',
-        'Adolescent fertility rate (births per 1,000 women ages 15-19)',
+        'International migrant stock (% of population)',
+        'Number of infant deaths',
         'Cause of death, by non-communicable diseases (% of total)']
 
+    # Create a subset of data with selected indicators
+    Mexico_subset = Mexico_data[indicators]
 
-# Create a subset of data with selected indicators
-    United_States_subset = United_States_data[indicators]
-
-# Plotting the heatmap
+    # Plotting the heatmap
     plt.figure(figsize=(12, 8))
-    heatmap = sns.heatmap(United_States_subset.corr(
-    ), annot=True, cmap='magma', fmt='.2f', annot_kws={"size": 10})
-    plt.title('Correlation Heatmap of Indicators for United States')
+    heatmap = sns.heatmap(Mexico_subset.corr(), annot=True,
+                          cmap='magma', fmt='.2f', annot_kws={"size": 10})
+
+    # Adjusting x-axis and y-axis labels
+    plt.xticks(rotation=45, ha='right')  # Rotating x-axis labels by 45 degrees
+    plt.yticks(rotation=0)  # Keeping y-axis labels horizontal
+
+    plt.title('Correlation Heatmap of Mexico')
+    plt.tight_layout()  # Adjust layout for better visualization
     plt.show()
 
-
 # Call the function and pass the CSV file path as an argument
-United_States_HeatMap('df_cleaned.csv')
+Mexico_HeatMap('df_cleaned.csv')
